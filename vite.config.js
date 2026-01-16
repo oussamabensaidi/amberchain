18f9bfb96fe4d4acccdea4d8ed2a161b83a28c45
oussama bensaidi <bensaidioussama7@gmail.com>
2026-01-16 17:37:07 +0100

    add alias refrence in the vite config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,28 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+
+    //for later use when im done 
+    //   "@app": path.resolve(__dirname, "src/app"),
+
+    //   // Shared building blocks
+    //   "@components": path.resolve(__dirname, "src/components"),
+    //   "@assets": path.resolve(__dirname, "src/assets"),
+    //   "@constants": path.resolve(__dirname, "src/constants"),
+
+    //   // Feature-based architecture
+    //   "@features": path.resolve(__dirname, "src/features"),
+
+    //   // Cross-cutting concerns
+    //   "@hooks": path.resolve(__dirname, "src/hooks"),
+    //   "@lib": path.resolve(__dirname, "src/lib"),
+    //   "@services": path.resolve(__dirname, "src/services"),
+    //   "@queries": path.resolve(__dirname, "src/queries"),
+    //   "@store": path.resolve(__dirname, "src/store"),
+    //   "@router": path.resolve(__dirname, "src/router"),
+    //   "@i18n": path.resolve(__dirname, "src/i18n"),
+      // "@mappers": path.resolve(__dirname, "src/mappers"),
+    // ,
     },
   },
   server: {
